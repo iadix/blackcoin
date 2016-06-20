60a214f7a9f005c6bf6577711ad404f03edd2789
root <root@s19215120.onlinehome-server.info>
2016-06-20 18:48:22 +0200

    oo

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -25,7 +25,8 @@ namespace Checkpoints
     //    timestamp before)
     // + Contains no strange transactions
     //
-    static MapCheckpoints mapCheckpoints =
+    static MapCheckpoints mapCheckpoints;
+        /*=
         boost::assign::map_list_of
         ( 5001,   uint256("0x2fac9021be0c311e7b6dc0933a72047c70f817e2eb1e01bede011193ad1b28bc") ) // hardfork
         ( 10000,  uint256("0x0000000000827e4dc601f7310a91c45af8df0dfc1b6fa1dfa5b896cb00c8767c") ) // last pow block
@@ -33,6 +34,7 @@ namespace Checkpoints
         ( 254348, uint256("0x9bf8d9bd757d3ef23d5906d70567e5f0da93f1e0376588c8d421a95e2421838b") ) // minor network split
         ( 319002, uint256("0x0011494d03b2cdf1ecfc8b0818f1e0ef7ee1d9e9b3d1279c10d35456bc3899ef") ) // hardfork
         ( 872456, uint256("0xe4fd321ced1de06213d2e246b150b4bfd8c4aa0989965dce88f2a58668c64860") ) // hardfork
+        */
     ;
 
     // TestNet has no checkpoints
