c3f41efcbdfe33722cd203c441a6400748722ec5
root <root@s19215120.onlinehome-server.info>
2016-06-21 14:28:03 +0200

    tt

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -131,7 +131,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        nLastPOWBlock = 10000;
+        nLastPOWBlock = 100;
     }
 
     virtual const CBlock& GenesisBlock() const { return genesis; }
